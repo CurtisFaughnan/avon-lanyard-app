755672e7d7672eeaa146a94053e36e649c302ad9
Curtis Faughnan <cwfaughn26@wabash.edu>
2026-03-03 19:12:40 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   appLabel: "Avon - Lanyard",
-  apiUrl: "https://script.google.com/macros/s/AKfycby7eXjDMv4Cxmfr4WLUQvlzSJF5sSsF6XqbaojRvtquUrbhY8s6rf9XlZmZK4xoQEHD/exec",
-  schoolKey: "AVON-LANYARD-2026-SECURE"
+  apiUrl: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec",
+  schoolKey: "AVON-LANYARD-2026-SECURE",
 };
